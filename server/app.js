8ae14715a89e4e6b8a3c3b9c3a5592aaa7671f7c
Nicholas Zaleski <nicholas.zaleski1@gmail.com>
2020-04-05 20:01:24 -0400

    found different url to connect to mongo because wasnt able to insert data with mongo+srv

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const app = express();
 const mongoUser = dotenv.config().parsed.MONGO_USER
 const mongoPW = dotenv.config().parsed.MONGO_PW
 
-mongoose.connect(`mongodb+srv://${mongoUser}:${mongoPW}@cluster0-cjiof.mongodb.net/test?retryWrites=true&w=majority`, { 
+mongoose.connect(`mongodb://${mongoUser}:${mongoPW}@cluster0-shard-00-00-cjiof.mongodb.net:27017,cluster0-shard-00-01-cjiof.mongodb.net:27017,cluster0-shard-00-02-cjiof.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`, { 
     useNewUrlParser: true, 
     useUnifiedTopology: true 
 })
