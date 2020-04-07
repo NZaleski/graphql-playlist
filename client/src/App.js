59a7f53f0e5fab4522b0f6ec88c97878b55459bf
Nicholas Zaleski <nicholas.zaleski1@gmail.com>
2020-04-07 19:07:29 -0400

    Added Apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+
+import BookList from './components/BookList'
+
+const client = new ApolloClient({
+  uri: 'http://localhost.com/4000/graphql'
+})
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <ApolloProvider client={client}>
+      <div id="Main">
+        <h1>Nic's Reading List</h1>
+        <BookList></BookList>
+      </div>
+    </ApolloProvider>
   );
 }
 
