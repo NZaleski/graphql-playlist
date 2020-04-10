345f452b25d505dc9b95c843449cdbd3f1979615
Nicholas Zaleski <nicholas.zaleski1@gmail.com>
2020-04-10 19:26:34 -0400

    Changed to function style on BookList component

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -3,21 +3,21 @@ import { useQuery } from '@apollo/react-hooks'
 import { BOOK_QUERY } from '../queries/queries'
 
 
-function BookList() {
-const { loading, error, data} = useQuery(BOOK_QUERY)
-  if (loading) return 'Loading...';
-  if (error) return `Error! ${error.message}`;
-  return (
-    <div>  
-        <ul id="book-list">
-        {data.books.map(book => (
-        <li key={book.id} value={book.name}>
-          {book.name}
-        </li>
-        ))}
-        </ul>
-    </div>
-  );
-}
+const BookList = () => {
+  const { loading, error, data} = useQuery(BOOK_QUERY)
+    if (loading) return 'Loading...';
+    if (error) return `Error! ${error.message}`;
+    return (
+      <div>  
+          <ul id="book-list">
+          {data.books.map(book => (
+          <li key={book.id} value={book.name}>
+            {book.name}
+          </li>
+          ))}
+          </ul>
+      </div>
+    );
+  }
 
 export default BookList
